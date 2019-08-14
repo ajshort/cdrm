4793a093f340d148da06874ad4a19ee10e5e5324
Andrew Short <andrewjshort@gmail.com>
2019-08-14 16:41:08 +1000

    Serialisation

--- cdrm/src/cdrm.cpp
+++ cdrm/src/cdrm.cpp
@@ -1,5 +1,16 @@
 #include <cdrm/cdrm.h>
 
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/graph/adj_list_serialize.hpp>
+#include <boost/graph/iteration_macros.hpp>
+#include <boost/serialization/array.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/serialization/vector.hpp>
+#include <ros/console.h>
+
+#include <fstream>
+
 namespace cdrm
 {
 Cdrm::Cdrm(double resolution) : resolution_(resolution)
@@ -26,11 +37,92 @@ Eigen::Vector3d Cdrm::keyToPoint(const Key &k) const
 
 bool Cdrm::save(const std::string &filename) const
 {
-  return false;
+  try
+  {
+    std::ofstream ofs(filename);
+    boost::archive::binary_oarchive oa(ofs);
+    oa << *this;
+  }
+  catch (const boost::archive::archive_exception &ex)
+  {
+    ROS_ERROR("%s", ex.what());
+    return false;
+  }
+
+  return true;
 }
 
 bool Cdrm::load(const std::string &filename)
 {
-  return false;
+  try
+  {
+    std::ifstream ifs(filename);
+    boost::archive::binary_iarchive ia(ifs);
+    ia >> *this;
+  }
+  catch (const boost::archive::archive_exception &ex)
+  {
+    ROS_ERROR("%s", ex.what());
+    return false;
+  }
+
+  return true;
+}
+}
+
+namespace boost
+{
+namespace serialization
+{
+template <class Archive>
+void serialize(Archive &ar, cdrm::Cdrm &cdrm, const unsigned int version)
+{
+  ar & cdrm.resolution_;
+  ar & cdrm.roadmap_;
+
+  ar & cdrm.colliding_vertices_;
+  ar & cdrm.colliding_edges_;
+  ar & cdrm.contacts_;
+
+  ar & cdrm.workspace_min_;
+  ar & cdrm.workspace_max_;
+
+  ar & cdrm.min_contact_distance_;
+  ar & cdrm.max_contact_distance_;
+}
+
+template <class Archive>
+void serialize(Archive &ar, cdrm::Vertex &v, const unsigned int version)
+{
+  ar & v.q_;
+}
+
+template <class Archive>
+void serialize(Archive &ar, cdrm::Edge &e, const unsigned int version)
+{
+  ar & e.cost_;
+}
+
+template <class Archive>
+void serialize(Archive &ar, Eigen::VectorXd &v, const unsigned int version)
+{
+  auto size = v.size();
+  ar & size;
+
+  v.resize(size);
+  ar & boost::serialization::make_array(v.data(), v.size());
+}
+
+template <class Archive>
+void serialize(Archive &ar, Eigen::Vector3d &v, const unsigned int version)
+{
+  ar & boost::serialization::make_array(v.data(), v.size());
+}
+
+template <class Archive>
+void serialize(Archive &ar, cdrm::EdgeDescriptor &ed, const unsigned int version)
+{
+  ar & ed;
+}
 }
 }
