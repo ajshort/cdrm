90e94b4436e3dd55f44271c5e3b1cfcac33cc8b4
Andrew Short <andrewjshort@gmail.com>
2019-08-25 16:26:08 +1000

    Fix up message generation

--- cdrm_welding/src/server.cpp
+++ cdrm_welding/src/server.cpp
@@ -1,8 +1,50 @@
+#include <cdrm_welding_msgs/PlanWeld.h>
+#include <moveit/robot_model/robot_model.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
 #include <ros/ros.h>
+
+namespace cdrm_welding
+{
+class Server
+{
+public:
+  Server(const moveit::core::RobotModelConstPtr &robot_model)
+    : nh_("~")
+    , robot_model_(robot_model)
+    , plan_server_(nh_, "plan_weld", std::bind(&Server::planWeld, this, std::placeholders::_1), false)
+  {
+    plan_server_.start();
+  }
+
+private:
+  void planWeld(const cdrm_welding_msgs::PlanWeldGoalConstPtr &goal)
+  {
+  }
+
+  ros::NodeHandle nh_;
+  moveit::core::RobotModelConstPtr robot_model_;
+
+  actionlib::SimpleActionServer<cdrm_welding_msgs::PlanWeldAction> plan_server_;
+  cdrm_welding_msgs::PlanWeldFeedback plan_feedback_;
+  cdrm_welding_msgs::PlanWeldResult plan_result_;
+};
+}
 
 int main(int argc, char *argv[])
 {
-  ros::init(argc, argv, "cdrm_server");
+  ros::init(argc, argv, "cdrm_welding_server");
   ros::NodeHandle node_handle;
+
+  robot_model_loader::RobotModelLoader loader("robot_description");
+  const auto &robot_model = loader.getModel();
+
+  if (!robot_model)
+  {
+    ROS_FATAL("The robot model could not be loaded from the robot_description param");
+    return -1;
+  }
+
+  cdrm_welding::Server server(robot_model);
+
   ros::spin();
 }
