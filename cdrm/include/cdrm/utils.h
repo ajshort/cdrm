b07b0f2bd08e99aa3d16d101118a216864641033
Andrew Short <andrewjshort@gmail.com>
2020-04-14 13:51:26 +1000

    Interpolate TF

--- cdrm/include/cdrm/utils.h
+++ cdrm/include/cdrm/utils.h
@@ -1,9 +1,16 @@
 #pragma once
 
+#include <Eigen/Geometry>
+
 #include <string>
 
 namespace cdrm
 {
+/**
+ * Linterp between two TFs.
+ */
+Eigen::Isometry3d interpolateTfs(const Eigen::Isometry3d &from, const Eigen::Isometry3d &to, double t);
+
 /**
  * Gets the ROS home directory.
  */
--- cdrm/src/utils.cpp
+++ cdrm/src/utils.cpp
@@ -4,6 +4,17 @@
 
 namespace cdrm
 {
+Eigen::Isometry3d interpolateTfs(const Eigen::Isometry3d &from, const Eigen::Isometry3d &to, double t)
+{
+  Eigen::Quaterniond from_quat(from.rotation());
+  Eigen::Quaterniond to_quat(to.rotation());
+
+  Eigen::Isometry3d result;
+  result.linear() = from_quat.slerp(t, to_quat).toRotationMatrix();
+  result.translation() = from.translation() + (to.translation() - from.translation()) * t;
+  return result;
+}
+
 std::string getRosHome()
 {
   std::string home;
