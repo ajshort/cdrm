057da843f55644d7c9085e921650db9df897f2a0
Andrew Short <andrewjshort@gmail.com>
2020-04-19 14:51:38 +1000

    Fix crash in edge serialisation

--- cdrm/include/cdrm/cdrm_serialisation.h
+++ cdrm/include/cdrm/cdrm_serialisation.h
@@ -14,22 +14,85 @@ namespace boost
 namespace serialization
 {
 template <class Archive>
+void save(Archive &ar, const cdrm::Cdrm &cdrm, const unsigned int version)
+{
+  ar << cdrm.resolution_;
+  ar << cdrm.roadmap_;
+
+  ar << cdrm.colliding_vertices_;
+
+  std::map<cdrm::VertexDescriptor, int> indices;
+  int index = 0;
+
+  BGL_FORALL_VERTICES_T(vertex, cdrm.roadmap_, cdrm::Roadmap)
+    indices[vertex] = index++;
+
+  int colliding_edge_size = cdrm.colliding_edges_.size();
+  ar << colliding_edge_size;
+
+  for (const auto &it : cdrm.colliding_edges_)
+  {
+    ar << it.first;
+    ar << indices[source(it.second, cdrm.roadmap_)];
+    ar << indices[target(it.second, cdrm.roadmap_)];
+  }
+
+  ar << cdrm.contacts_;
+
+  ar << cdrm.aabb_;
+
+  ar << cdrm.workspace_min_;
+  ar << cdrm.workspace_max_;
+
+  ar << cdrm.min_contact_distance_;
+  ar << cdrm.max_contact_distance_;
+}
+
+template <class Archive>
+void load(Archive &ar, cdrm::Cdrm &cdrm, const unsigned int version)
+{
+  ar >> cdrm.resolution_;
+  ar >> cdrm.roadmap_;
+
+  ar >> cdrm.colliding_vertices_;
+
+  std::map<cdrm::VertexDescriptor, int> indices;
+  int index = 0;
+
+  BGL_FORALL_VERTICES_T(vertex, cdrm.roadmap_, cdrm::Roadmap)
+    indices[vertex] = index++;
+
+  int colliding_edge_size;
+  ar >> colliding_edge_size;
+
+  for (unsigned int i = 0; i < colliding_edge_size; ++i)
+  {
+    cdrm::Key key;
+    int source_index, target_index;
+    ar >> key >> source_index >> target_index;
+
+    auto source = indices[source_index];
+    auto target = indices[target_index];
+    auto edge_descriptor = boost::edge(source, target, cdrm.roadmap_).first;
+
+    cdrm.colliding_edges_.insert(std::make_pair(key, edge_descriptor));
+  }
+
+  ar >> cdrm.contacts_;
+
+  ar >> cdrm.aabb_;
+
+  ar >> cdrm.workspace_min_;
+  ar >> cdrm.workspace_max_;
+
+  ar >> cdrm.min_contact_distance_;
+  ar >> cdrm.max_contact_distance_;
+}
+
+template <class Archive>
 void serialize(Archive &ar, cdrm::Cdrm &cdrm, const unsigned int version)
 {
-  ar & cdrm.resolution_;
-  ar & cdrm.roadmap_;
-
-  ar & cdrm.colliding_vertices_;
-  ar & cdrm.colliding_edges_;
-  ar & cdrm.contacts_;
-
-  ar & cdrm.aabb_;
-
-  ar & cdrm.workspace_min_;
-  ar & cdrm.workspace_max_;
-
-  ar & cdrm.min_contact_distance_;
-  ar & cdrm.max_contact_distance_;
+  split_free(ar, cdrm, version);
 }
 
 template <class Archive>
@@ -51,13 +114,13 @@ void serialize(Archive &ar, Eigen::VectorXd &v, const unsigned int version)
   ar & size;
 
   v.resize(size);
-  ar & boost::serialization::make_array(v.data(), v.size());
+  ar & make_array(v.data(), v.size());
 }
 
 template <class Archive>
 void serialize(Archive &ar, Eigen::Vector3d &v, const unsigned int version)
 {
-  ar & boost::serialization::make_array(v.data(), v.size());
+  ar & make_array(v.data(), v.size());
 }
 
 template <class Archive>
